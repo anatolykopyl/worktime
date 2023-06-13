f6c480908a42f61b208b1ec625ca6621320a3815
anatolykopyl <anatolykopyl@users.noreply.github.com>
2023-06-13 21:22:50 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.19c7694d5efebf9bd7ad68e779b4a1e6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d943b1da3920bd849486",
+    "url": "/worktime/css/app.cc9f3f0e.css"
+  },
+  {
+    "revision": "df13156bbaa7c8bbc9ef7f7ce6151e2c",
+    "url": "/worktime/img/cross.df13156b.svg"
+  },
+  {
+    "revision": "944f434bffd9502c966cd45fcb92be9b",
+    "url": "/worktime/img/gear.944f434b.svg"
+  },
+  {
+    "revision": "70a067714bae479e5088379cd01d3a92",
+    "url": "/worktime/img/pause.70a06771.svg"
+  },
+  {
+    "revision": "53d47113b1369fdfabb5741b3786bdef",
+    "url": "/worktime/img/play.53d47113.svg"
+  },
+  {
+    "revision": "2a2e9e9d6aec82c8516059ac722fbd64",
+    "url": "/worktime/img/reset.2a2e9e9d.svg"
+  },
+  {
+    "revision": "8a25249241f7edec9d74d1f89aa3d026",
+    "url": "/worktime/img/stop.8a252492.svg"
+  },
+  {
+    "revision": "c93e9df1e70ca77760eb90340bb1ed0a",
+    "url": "/worktime/img/trash.c93e9df1.svg"
+  },
+  {
+    "revision": "4e241e31c0e913dc9b2792bea215dbb0",
+    "url": "/worktime/index.html"
+  },
+  {
+    "revision": "d943b1da3920bd849486",
+    "url": "/worktime/js/app.d67a7dd4.js"
+  },
+  {
+    "revision": "efee94a9762c62269ff4",
+    "url": "/worktime/js/chunk-vendors.d19d72f8.js"
+  },
+  {
+    "revision": "b04dbd24300adc20845a6594017d9225",
+    "url": "/worktime/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/worktime/robots.txt"
+  }
+]);
